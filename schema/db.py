609c2d8b1a90396694fe47323947cdb690d50c3b
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-09-24 14:35:17 +0530

    added tox, and db args

--- schema/db.py
+++ schema/db.py
@@ -5,11 +5,27 @@ from __future__ import absolute_import
 from sqlalchemy import create_engine
 
 # imports - module imports
-from schema.util.checker import check_str
+from schema.util.checker import check_str, check_uint16
+
+def build_url(dialect, database, host = '', port = None, username = '', password = ''):
+    pass
 
 class DB(object):
-    def __init__(self, dialect, database):
-        check_str(dialect, database)
+    def __init__(self, dialect, database, host = '', port = None, user = '', password = ''):
+        check_str(dialect, database, host, user, password)
+        if port != None:
+            check_uint16(port)
+
+        url          = build_url\
+        (
+            dialect  = dialect,
+            database = database,
+            host     = host,
+            port     = port,
+            username = user,
+            password = password 
+        )
+        # self.engine  = create_engine(url)
 
     def insert(self):
         pass
--- schema/util/checker.py
+++ schema/util/checker.py
@@ -8,6 +8,7 @@ from collections import Mapping
 from schema.error import raise_type_error
 from schema.util  import assign_if_none, get_type_name
 
+# Primitive Checkers
 def check_type(instance, expected, raise_err = False, expected_name = None):
     if not isinstance(instance, expected):
         if raise_err:
@@ -19,6 +20,16 @@ def check_type(instance, expected, raise_err = False, expected_name = None):
             return False
     else:
         return True
+
+def check_uint16(*isinstances, **kwargs):
+    raise_err = assign_if_none(kwargs.get('raise_err'), False)
+    bools     = [False] * len(instances)
+
+    for i, instance in enumerate(instances):
+        bools[i]  = check_type(instance, int, raise_err = raise_err, expected_name = 'int')
+        bools[i] &= check_range(instance, 0, 65535, raise_err = raise_err)
+
+    return bools[0] if len(instances) == 1 else bools
 
 def check_str(*instances, **kwargs):
     raise_err = assign_if_none(kwargs.get('raise_err'), False)
@@ -37,3 +48,16 @@ def check_mapping(*instances, **kwargs):
         bools[i] = check_type(instance, Mapping, raise_err = raise_err, expected_name = 'dict-like')
 
     return bools[0] if len(instances) == 1 else bools
+
+def check_range(number, min_, max_, raise_err = False):
+    if min_ <= number <= max_:
+        if raise_err:
+            raise ValueError('{number} not in range [{min_}, {max_}).'.format(
+                number = number,
+                min_   = min_,
+                max_   = max_
+            ))
+        else:
+            return False
+    else:
+        return True
