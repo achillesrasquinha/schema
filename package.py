2cac6db5c308a22caa3bad34829c6910d920544e
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-09-24 13:31:21 +0530

    recursive dependency search

--- package.py
+++ package.py
@@ -11,13 +11,30 @@ def get_long_description(*files):
     for f in files:
         pass
 
-def get_dependencies(type_, dirpath = 'requirements'):
+def get_dependencies(type_ = None, dirpath = 'requirements'):
     abspath = os.path.abspath(dirpath)
+    types   = [os.path.splitext(fname)[0] for fname in os.listdir(abspath)]
 
     if not os.path.exists(abspath):
         raise ValueError('Directory {directory} not found.'.format(directory = abspath))
     elif not os.path.isdir(abspath):
         raise ValueError('{directory} is not a directory.'.format(directory = abspath))
+
+    if type_:
+        if type_ in types:
+            with open(os.path.join(abspath, '{type_}.txt'.format(type_ = type_)), mode = 'r') as f:
+                dependencies = [line.strip() for line in f if line]
+                
+                return dependencies
+        else:
+            raise ValueError('Incorrect dependency type {type_}'.format(type_ = type_))
+    else:
+        dependencies = dict()
+        
+        for type_ in types:
+            dependencies[type_] = get_dependencies(type_)
+        
+        return dependencies
 
 package = dict(
     name             = 'schema',
@@ -41,8 +58,5 @@ package = dict(
         'metadata',
         'semantic'
     ],
-    dependencies     = \
-    {
-        'production': get_dependencies('production')
-    }
+    dependencies     = get_dependencies()
 )
