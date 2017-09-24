97708bf3131e8aa87348fa3de09e841f48fbc982
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-09-24 11:56:13 +0530

    [FIX] multiple args with default args for python 2 and 3

--- schema/util/checker.py
+++ schema/util/checker.py
@@ -6,7 +6,7 @@ from collections import Mapping
 
 # imports - module imports
 from schema.error import raise_type_error
-from schema.util  import get_type_name
+from schema.util  import assign_if_none, get_type_name
 
 def check_type(instance, expected, raise_err = False, expected_name = None):
     if not isinstance(instance, expected):
@@ -20,16 +20,18 @@ def check_type(instance, expected, raise_err = False, expected_name = None):
     else:
         return True
 
-def check_str(*instances, raise_err = False):
-    bools = [False] * len(instances)
+def check_str(*instances, **kwargs):
+    raise_err = assign_if_none(kwargs.get('raise_err'), False)
+    bools     = [False] * len(instances)
 
     for i, instance in enumerate(instances):
         bools[i] = check_type(instance, six.string_types, raise_err = raise_err, expected_name = 'str')
 
     return bools[0] if len(instances) == 1 else bools
 
-def check_mapping(*instances, raise_err = False):
-    bools = [False] * len(instances)
+def check_mapping(*instances, **kwargs):
+    raise_err = assign_if_none(kwargs.get('raise_err'), False)
+    bools     = [False] * len(instances)
 
     for i, instance in enumerate(instances):
         bools[i] = check_type(instance, Mapping, raise_err = raise_err, expected_name = 'dict-like')
